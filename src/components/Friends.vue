815380805d232eade563f700a6715fbee6d970ab
NbekWeb <nbekdev@gmail.com>
2024-09-20 11:56:36 +0500

    rating

--- src/components/Friends.vue
+++ src/components/Friends.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="px-5 pt-10">
-    <div class="flex flex-col items-center gap-3 mb-4 text-center text-white">
+  <div class="px-5 pt-10 text-white">
+    <div class="flex flex-col items-center gap-3 mb-4 text-center">
       <span class="text-2xl font-semibold">Приглашайте друзей!</span>
       <span class="text-sm">Вы и ваш друг получите бонусы</span>
       <div
@@ -10,26 +10,88 @@
         <user />
       </div>
     </div>
-    <div class="flex flex-col gap-2 text-white">
-      <div class="flex gap-8 p-3 bg-white-13 rounded-3xl">
-        <img src="@/assets/img/prize.png" class="w-10 " />
+    <div class="flex flex-col gap-2 mb-4">
+      <div class="flex gap-8 p-3 bg-blue-700 rounded-3xl">
+        <img src="@/assets/img/prize.png" class="w-10" />
         <div class="flex flex-col gap-1">
           <span class="text-lg font-semibold">Пригласить друга</span>
           <div class="flex gap-2 text-sm">
             <img src="@/assets/img/coin-vpn.png" class="w-6 h-6" />
-            <span class="font-semibold">+ 5000 </span>
-            <span>для вас и вашего друга</span>
+            <span class="font-semibold">+ 5 000 </span>
+            <span class="text-xs">для вас и вашего друга</span>
           </div>
         </div>
+      </div>
+      <div class="flex gap-4 p-3 bg-blue-700 rounded-3xl">
+        <img src="@/assets/img/prizes.png" class="h-auto w-15" />
+        <div class="flex flex-col gap-1">
+          <span class="text-lg font-semibold line-1"
+            >Пригласить друга с Telegram Premium
+          </span>
+          <div class="flex gap-2 text-sm">
+            <img src="@/assets/img/coin-vpn.png" class="w-6 h-6" />
+            <span class="font-semibold">+ 25 000 </span>
+            <span class="text-xs">для вас и вашего друга</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="flex items-center justify-between mb-2 text-lg font-semibold">
+        <span>Список ваших друзей </span>
+        <rotate />
+      </div>
+      <div class="flex flex-col gap-2 pb-4">
+        
+          <div
+            class="flex items-center justify-between p-3 rounded-2xl bg-white-13"
+          >
+            <div class="flex items-center gap-5 text-xl">
+              <img src="@/assets/img/avatar-minion.png" class="w-8 h-8" />
+              <span>Nik name</span>
+            </div>
+            <div class="flex items-center gap-2 text-sm font-medium">
+              <img src="@/assets/img/coin-vpn.png" class="w-6 h-6" />
+              <span> +200</span>
+            </div>
+          </div>
+          <div
+            class="flex items-center justify-between p-3 rounded-2xl bg-white-13"
+          >
+            <div class="flex items-center gap-5 text-xl">
+              <img src="@/assets/img/avatar.png" class="w-8 h-8" />
+              <span>Nik name</span>
+            </div>
+            <div class="flex items-center gap-2 text-sm font-medium">
+              <img src="@/assets/img/coin-vpn.png" class="w-6 h-6" />
+              <span> +200</span>
+            </div>
+          </div>
+          <div
+            class="flex items-center justify-between p-3 rounded-2xl bg-white-13"
+          >
+            <div class="flex items-center gap-5 text-xl">
+              <img src="@/assets/img/anonim.png" class="w-8 h-8" />
+              <span>Nik name</span>
+            </div>
+            <div class="flex items-center gap-2 text-sm font-medium">
+              <img src="@/assets/img/coin-vpn.png" class="w-6 h-6" />
+              <span> +200</span>
+            </div>
+          </div>
       </div>
     </div>
   </div>
 </template>
 <script setup>
 import user from "@/components/icons/user.vue";
+import Rotate from "./icons/rotate.vue";
 </script>
-<style scoped>
+<style>
 .green-linear {
   background: linear-gradient(90deg, #48e8a4 0%, #59b9c6 100%);
+}
+.line-1 {
+  line-height: 1;
 }
 </style>
